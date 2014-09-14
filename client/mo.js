a2c78c393545e531dfca37e0f1f2889b4878bf84
hikarock <i@hika69.com>
2014-09-14 14:01:30 +0900

    isClientでpusher動かすのいったん諦めてインラインにて..

--- client/mo.js
+++ client/mo.js
@@ -1,19 +1,5 @@
 if (Meteor.isClient) {
-  setInterval(function() {
-    var width = $('body').width();
-    var height = $('body').height() + 150;
-    var left = getRandomInt(0, width - 75);
-    $('.coin').animate({
-      'top': height,
-      'left': left
-    }, 1500, function() {
-      $('.coin').css('top', '-150');
-    });
-  }, 1000);
-
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  //
 }
 
 if (Meteor.isServer) {
